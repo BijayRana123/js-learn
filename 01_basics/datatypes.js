c4ee3919369afdde0fdb08c39dbcf56e668c87b5
BijayRana123 <52782467+BijayRana123@users.noreply.github.com>
2024-12-04 07:20:58 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all the code as newer version
+//alert(3+3) we are using in nodejs not in the browser
+
+let isLoggedIn = false;
+let age = 32;
+let temp;
+
+//number
+//bigint
+//boolean
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+console.log(typeof age); //number
+console.log(typeof isLoggedIn);
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
