28d3e78ef2f8918175a712060f029b130c44de29
BijayRana123 <52782467+BijayRana123@users.noreply.github.com>
2024-12-04 07:02:09 +0000

    variables added

---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 112233;
+let accountEmail = "bijay@yahoo.com";
+var accountPass = "12345";
+accountCity = "Butwal";
+let accountState;
+
+// accountId = 4;
+// Please dont use var for variable because of the scope problem in block and the function defined
+accountEmail = "king@pass.com";
+console.log(accountId);
+console.table([accountId,accountEmail, accountPass,accountCity, accountState]);
