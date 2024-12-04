764dabdf40e9ad712399099349ce750c87ae66df
BijayRana123 <52782467+BijayRana123@users.noreply.github.com>
2024-12-04 16:06:43 +0000

    summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,31 @@
+// Primitive
+
+//7 types => Number, String, Boolean, null, undefined, BigInt, Symbol
+ 
+const score = 45;
+const scoreVal = 100.344;
+
+const isLogIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(id === anotherId);
+
+const bigNumber = 123456753423355n;   //became BigInt
+
+const heros = ["batman", "spiderman", "wolverine"];
+let myObj = {
+    name: "Batman",
+    age: 50,
+};
+const myFunction = function () {
+    console.log("Hello World!");
+}
+
+
+
+//Reference(Non-Primitive)
+//Array, Objects, Functions
+
