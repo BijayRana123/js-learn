e663bc6c739d2f3b9f300e9c4bab8bfd52a07407
BijayRana123 <52782467+BijayRana123@users.noreply.github.com>
2024-12-04 15:50:34 +0000

    comparison learned

---
+++ 01_basics/comparison.js
@@ -0,0 +1,28 @@
+// console.log( 2> 1);
+// console.log( 2>= 1);
+// console.log( 2== 1);
+// console.log( 2<=1);
+// console.log( 2< 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null == 0);  *
+// console.log(null >= 0);  *
+// console.log(null < 0);   *
+
+// console.log(undefined == 0); *
+// console.log(undefined > 0);  *
+// console.log(undefined < 0);  *
+
+// try to avoid * types comparison they are inconsistent if you have to use , typescript is recommended
+
+// === strict check
+
+console.log("2" == 2);
+console.log("2" === 2);
+
+
+
+
+
