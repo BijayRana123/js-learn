627ca4f9ce197c4a129a53efc66a1106ea4e64d0
BijayRana123 <52782467+BijayRana123@users.noreply.github.com>
2024-12-05 17:02:29 +0000

    scopes done

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,28 @@
+let c = 300;
+if (true){
+    let a =10;
+    let c = 30;
+    // console.log("Inner: ",c);
+};
+// console.log(c);
+
+// function one(){
+//     const username = "Bijay";
+//     function two() {
+//         const website = "youtube";
+//         console.log(username);
+//     };
+//     // console.log(website);
+//     two();
+// }
+// one();
+
+// function addOne(num) {
+//     return num + 1;
+// };
+// addOne(2);
+
+const addOne = function(num) {
+    return num + 1;
+};
+addOne(2);
