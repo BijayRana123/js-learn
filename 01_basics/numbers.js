44678b07d2a191373a282747f2103e5555c58f4a
BijayRana123 <52782467+BijayRana123@users.noreply.github.com>
2024-12-05 08:06:17 +0000

    math&num

---
+++ 01_basics/numbers.js
@@ -0,0 +1,41 @@
+// const score =400;
+// console.log(score);
+
+// const num = new Number(100.20);
+// console.log(num);
+// console.log(num.toString());
+// console.log(num.toString().length);
+// console.log(num.toFixed(2));
+
+// const otherNum = 123.8546;
+// console.log(otherNum.toPrecision(3));
+// const anNum = 23.456;
+// console.log(anNum.toPrecision(3));
+// const ananNum = 1123.4562;
+// console.log(ananNum.toPrecision(3));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+/******************Maths*************** */
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(2.345));
+// console.log(Math.ceil(2.345));
+// console.log(Math.floor(2.945));
+// console.log(Math.min(2,4,6,8,9));
+// console.log(Math.max(2,4,7,9));
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const max = 20;
+const min = 10;
+console.log(Math.floor(Math.random() * (max - min + 1))+min);
+
+
+
+
