61e0ee337f18c0e15666a8495ff29920125a5ee2
BijayRana123 <52782467+BijayRana123@users.noreply.github.com>
2024-12-06 07:48:50 +0000

    switch done

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,21 @@
+const month = 8;
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Februaru");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    default:
+        console.log("Beyond May");
+        break;
+};
