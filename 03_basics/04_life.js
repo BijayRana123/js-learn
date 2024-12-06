8ef06f6f6fb9d39a20fa10412911a7108a0e75e5
BijayRana123 <52782467+BijayRana123@users.noreply.github.com>
2024-12-06 06:30:52 +0000

    life done

---
+++ 03_basics/04_life.js
@@ -0,0 +1,16 @@
+//Immediately Invoked Function Expression(IIFE)
+
+// function chai(){
+//     console.log(`DB Connected`);
+// }
+// chai();
+// (function chai() {
+//     console.log(`DB Connected`);    IIFE
+// })();
+// (()=>{
+//     console.log(`DB Connected 2`);  IIFE for arrow function
+// })();
+// ((user) => {
+//     console.log(`Connected with ${user}`);
+// })('Bijay');
+
