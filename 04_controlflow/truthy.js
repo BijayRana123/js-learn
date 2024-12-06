8675dd9d6c49e9fedf0b60a56d29f6af1434ae29
BijayRana123 <52782467+BijayRana123@users.noreply.github.com>
2024-12-06 08:13:40 +0000

    truthy done

---
+++ 04_controlflow/truthy.js
@@ -0,0 +1,32 @@
+// const userEmail = "h@hasd.ai";
+// if (userEmail) {
+//     console.log("Got User Email");
+// }else {
+//     console.log("Didnt got any email");
+// };
+
+// //flasy value => 0,null, undefined, -0, false,"", BigInt 0n,NaN
+// //truthy value => '0','false',{}," ",[],function(){}
+
+// const arr = [];
+// if (arr.length === 0) {
+//     console.log("Array is empty");
+// } else {
+//     console.log("Array isnt empty");
+// };
+
+// const emptyObj = {};
+// if (Object.keys(emptyObj.length ===0)) {
+//     console.log("Object is empty");
+// } else {
+//     console.log("Object isnt empty");
+// };
+
+//Nullish Coalescing Operator(??): null undefined
+// let val1;
+// // val1 = 5??10;
+// val1 = null??10;
+// console.log(val1);
+
+//Terinary Operator
+// condition ? true: false;
