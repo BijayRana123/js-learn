1bd2a4397805d3af62781d6ae7b9c2e2f4b0e77b
BijayRana123 <52782467+BijayRana123@users.noreply.github.com>
2024-12-06 11:07:21 +0000

    done with filters

---
+++ 05_iteration/five.js
@@ -0,0 +1,12 @@
+const myNum = [1,2,3,4,5,6,7,8,9,10];
+// const newNum = myNum.filter((num) => num >4);
+// console.log(newNum);
+
+// const nNum = [];
+// myNum.forEach((num) => {
+//     if (num>4) {
+//         nNum.push(num);
+//     };
+// });
+// console.log(nNum);
+
